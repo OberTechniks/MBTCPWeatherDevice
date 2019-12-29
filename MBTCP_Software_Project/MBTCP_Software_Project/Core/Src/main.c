b78014d541971a69fbe47a380fd23bc2fdb1dfff
OberTechniks <obertechniks@gmail.com>
2019-12-29 10:45:52 -0500

    Various board bring-up work.

--- MBTCP_Software_Project/MBTCP_Software_Project/Core/Src/main.c
+++ MBTCP_Software_Project/MBTCP_Software_Project/Core/Src/main.c
@@ -45,6 +45,8 @@
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc;
 
+I2C_HandleTypeDef hi2c1;
+
 SPI_HandleTypeDef hspi1;
 
 UART_HandleTypeDef huart2;
@@ -57,6 +59,7 @@ UART_HandleTypeDef huart2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_ADC_Init(void);
+static void MX_I2C1_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
@@ -98,48 +101,33 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_ADC_Init();
+  MX_I2C1_Init();
   MX_SPI1_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
 
-  uint32_t timeout = 10000;
+  //**************************************************************************************************************
+  // HAL Items
+  uint8_t HAL_OK_Buffer[6] = {72, 65, 76, 32, 79, 75};								// Buffer for HAL_OK message
+  uint8_t HAL_ERROR_Buffer[9] = {72, 65, 76, 32, 69, 82, 82, 79, 82};				// Buffer for HAL_ERROR message
+  uint8_t HAL_BUSY_Buffer[8] = {72, 65, 76, 32, 66, 85, 83, 89};					// Buffer for HAL_BUSY message
+  uint8_t HAL_TIMEOUT_Buffer[11] = {72, 65, 76, 32, 84, 73, 77, 69, 79, 85, 84};	// Buffer for HAL_TIMEOUT message
+  uint8_t crlfBuffer[2] = {13, 10};													// Buffer for carriage return and line feed characters
+  HAL_StatusTypeDef UART2_STATUS;													// UART2 status object
+  HAL_StatusTypeDef I2C1_STATUS;													// I2C1 status object
+  uint32_t timeout = 10000;															// HAL timeout value
+  // End HAL Items
+  //*************************************************************************************************************
 
-  // turn on the led
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
+  //***************************************************************************************************************
+  // Begin board bring-up specific items
+  uint8_t message_ChargeGood[11] = {67, 104, 97, 114, 103, 101, 32, 71, 79, 79, 68};
+  uint8_t message_ChargeBad[10] = {67, 104, 97, 114, 103, 101, 32, 66, 65, 68};
+  uint8_t message_FaultGood[10] = {70, 97, 117, 108, 116, 32, 71, 79, 79, 68};
+  uint8_t message_FaultBad[9] = {70, 97, 117, 108, 116, 32, 66, 65, 68};
 
-  // buffer for HAL_OK message
-  uint8_t HAL_OK_Buffer[6] = {72, 65, 76, 32, 79, 75};
-
-  // buffer for HAL_ERROR message
-  uint8_t HAL_ERROR_Buffer[9] = {72, 65, 76, 32, 69, 82, 82, 79, 82};
-
-  // buffer for HAL_BUSY message
-  uint8_t HAL_BUSY_Buffer[6] = {72, 65, 76, 32, 66, 85, 83, 89};
-
-  // buffer for HAL_TIMEOUT message
-  uint8_t HAL_TIMEOUT_Buffer[11] = {72, 65, 76, 32, 84, 73, 77, 69, 79, 85, 84};
-
-  // buffer for carriage return and line feed characters
-  uint8_t crlfBuffer[2] = {13, 10};
-
-  // UART2 status object
-  HAL_StatusTypeDef UART2_STATUS;
-
-  // SPI1 status object
-  HAL_StatusTypeDef SPI1_STATUS;
-
-  // FRAM SPI Op-Codes
-  uint8_t WREN = 0x06;	// Set Write Enable Latch, must be set before WRSR or WRITE commands are executed
-  uint8_t WRDI = 0x04;	// Reset Write Enable Latch, WRSR and WRITE commands are not executed while Write Enable is reset
-  uint8_t RDSR = 0x05;	// Read Status Register
-  uint8_t WRSR = 0x01;	// Write Status Register
-  uint8_t READ = 0x03;	// Read memory code
-  uint8_t WRITE = 0x02; // Write memory code
-  uint8_t RDID = 0x9F;	// Read Device ID
-  char data[6] = "";
-
-  uint8_t TxData[5] = {159, 8, 8, 8, 8};
-  uint8_t RxData[5] = {255, 255, 255, 255, 255};
+  // End board bring-up specific items
+  //***************************************************************************************************************
 
   /* USER CODE END 2 */
 
@@ -147,43 +135,36 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
+	  // Toggle the led
+	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_5);
 
-	  // SPI transaction
-	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_11, GPIO_PIN_RESET);	// set chip select low
-	  SPI1_STATUS = HAL_SPI_TransmitReceive(&hspi1, &TxData, &RxData, 5, timeout);
+	  // Delay a bit before looping
+	  HAL_Delay(1000);
 
-	  // End the SPI transaction
-	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_11, GPIO_PIN_SET);	// set chip select high
-
-	  // Evaluate if HAL functions were successful
-	  if(SPI1_STATUS == HAL_OK)
+	  // Check the status of the /CHRG and /FAULT pins, message accordingly
+	  if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_0) == GPIO_PIN_SET)
 	  {
-		  // Seems like it was successful
-		  // Extract the data and send the data out the serial port
-		  UART2_STATUS = HAL_UART_Transmit(&huart2, HAL_OK_Buffer, sizeof(HAL_OK_Buffer), timeout);
-		  UART2_STATUS = HAL_UART_Transmit(&huart2, crlfBuffer, sizeof(crlfBuffer), timeout);
-
-		  //sprintf(data, "%d", RxData);
-		  UART2_STATUS = HAL_UART_Transmit(&huart2, RxData, sizeof(RxData), timeout);
+		  // Charging pin is good
+		  UART2_STATUS = HAL_UART_Transmit(&huart2, message_ChargeGood, sizeof(message_ChargeGood), timeout);
 		  UART2_STATUS = HAL_UART_Transmit(&huart2, crlfBuffer, sizeof(crlfBuffer), timeout);
 	  }
-	  else if(SPI1_STATUS == HAL_ERROR)
+	  else
 	  {
-		  // There was an error, send it out the serial port
-		  UART2_STATUS = HAL_UART_Transmit(&huart2, HAL_ERROR_Buffer, sizeof(HAL_ERROR_Buffer), timeout);
+		  // Charging pin is not good
+		  UART2_STATUS = HAL_UART_Transmit(&huart2, message_ChargeBad, sizeof(message_ChargeBad), timeout);
 		  UART2_STATUS = HAL_UART_Transmit(&huart2, crlfBuffer, sizeof(crlfBuffer), timeout);
 	  }
-	  else if(SPI1_STATUS == HAL_BUSY)
+
+	  if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_1) == GPIO_PIN_SET)
 	  {
-		  // HAL is busy, send it out the serial port
-		  UART2_STATUS = HAL_UART_Transmit(&huart2, HAL_BUSY_Buffer, sizeof(HAL_BUSY_Buffer), timeout);
+		  // Fault pin is good
+		  UART2_STATUS = HAL_UART_Transmit(&huart2, message_FaultGood, sizeof(message_FaultGood), timeout);
 		  UART2_STATUS = HAL_UART_Transmit(&huart2, crlfBuffer, sizeof(crlfBuffer), timeout);
 	  }
-	  else if(SPI1_STATUS == HAL_TIMEOUT)
+	  else
 	  {
-		  // There was a HAL timeout, send it out the serial port
-		  UART2_STATUS = HAL_UART_Transmit(&huart2, HAL_TIMEOUT_Buffer, sizeof(HAL_TIMEOUT_Buffer), timeout);
+		  // Fault pin is bad
+		  UART2_STATUS = HAL_UART_Transmit(&huart2, message_FaultBad, sizeof(message_FaultBad), timeout);
 		  UART2_STATUS = HAL_UART_Transmit(&huart2, crlfBuffer, sizeof(crlfBuffer), timeout);
 	  }
 
@@ -192,8 +173,7 @@ int main(void)
 		  // do nothing, just to avoid a build warning
 	  }
 
-	  // Delay a while before trying again
-	  HAL_Delay(2000);
+    /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
   }
@@ -236,8 +216,9 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_I2C1;
   PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
@@ -295,6 +276,52 @@ static void MX_ADC_Init(void)
   /* USER CODE BEGIN ADC_Init 2 */
 
   /* USER CODE END ADC_Init 2 */
+
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x00303D5B;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Analogue filter 
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Digital filter 
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
 
 }
 
@@ -395,10 +422,15 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PC0 PC1 PC6 PC7 
+                           PC8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_6|GPIO_PIN_7 
+                          |GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PC3 PC5 */
   GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_5;
@@ -414,25 +446,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PC6 PC7 */
-  GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
   /*Configure GPIO pins : PC9 PC10 PC11 */
   GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PA10 */
-  GPIO_InitStruct.Pin = GPIO_PIN_10;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PB5 */
   GPIO_InitStruct.Pin = GPIO_PIN_5;
